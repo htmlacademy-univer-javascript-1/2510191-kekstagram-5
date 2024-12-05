da53bd8f7b96b47948e47204d4e345cce55262e2
Liza <kekch13@gmail.com>
2024-12-05 22:59:12 +0500

    9.13. Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { generatePhoto } from './generate-photo.js';
 import { renderPictures } from './gallery.js';
+import './form.js';
+
 const posts = [];
 for (let i = 1; i <= 25; i++) {
   const currentPhoto = generatePhoto(i);
--- js/util.js
+++ js/util.js
@@ -2,6 +2,6 @@ function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const isEscapeKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
 export { getRandomInteger, isEscapeKey};
