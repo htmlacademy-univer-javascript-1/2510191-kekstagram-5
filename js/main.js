bd44df270f4e7bf0a8918a6839908522471286a2
Liza <kekch13@gmail.com>
2025-01-02 23:03:32 +0500

    12.12. Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-import { generatePhoto } from './generate-photo.js';
-import { renderPictures } from './gallery.js';
-import './form.js';
-
-const posts = [];
-for (let i = 1; i <= 25; i++) {
-  const currentPhoto = generatePhoto(i);
-  posts.push(currentPhoto);
-}
-
-renderPictures(posts);import { showFilteredPictures } from './filter.js';
+import { showFilteredPictures } from './filter.js';
 import { getData } from './api.js';
 import { showAlert } from './util.js';
 import './form.js';
